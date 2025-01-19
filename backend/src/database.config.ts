ec26c14bd8d4565c28ca73445bfdbf4ea020ba80
Hasmik-icml <gevorgyan.icml@gmail.com>
2025-01-19 10:37:54 +0400

    Create db config

---
+++ backend/src/database.config.ts
@@ -0,0 +1,16 @@
+import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: parseInt(process.env.POSTGRES_DB_PORT || '5432', 10),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    synchronize: true,
+    logging: true,
+    entities: ['src/entities/*.ts']
+});
