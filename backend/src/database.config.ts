6e2a8a27644b6e14318bf1186a57f6af27ce3046
Hasmik-icml <gevorgyan.icml@gmail.com>
2025-01-19 10:54:32 +0400

    Fix the bug with hos name in datasource

--- backend/src/database.config.ts
+++ backend/src/database.config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "db",
     port: parseInt(process.env.POSTGRES_DB_PORT || '5432', 10),
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
