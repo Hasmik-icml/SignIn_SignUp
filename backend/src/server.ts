530d6b7080104e0653c94e4f2d5bb721b20f78ff
Hasmik-icml <gevorgyan.icml@gmail.com>
2025-01-19 10:49:02 +0400

    Initialize datasource

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,21 @@
 import express, {Request, Response} from "express";
+import { AppDataSource } from "./database.config";
  
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
+
+app.use('/', router);
+
 app.listen(port, () => {
     console.log(`server is running on ${port}`);
 })
