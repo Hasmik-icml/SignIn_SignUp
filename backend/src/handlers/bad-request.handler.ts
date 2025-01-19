fed6a069070cfcdb3e2528f1da830a4498e23dc7
Hasmik-icml <gevorgyan.icml@gmail.com>
2025-01-19 11:54:08 +0400

    Create bad request error handler

---
+++ backend/src/handlers/bad-request.handler.ts
@@ -0,0 +1,15 @@
+import { ValidationError } from "express-validator";
+import { CustomError } from "../errors/custom.error";
+
+export class BadRequestError extends CustomError {
+    statusCode = 400;
+
+    constructor(public errors: string) {
+        super(typeof errors === 'string' ? errors : 'Invalid request parameters');
+        Object.setPrototypeOf(this, BadRequestError.prototype);
+    }
+
+    serializeErrors(): { message: string; field?: string; }[] {
+        return [{ message: this.errors }];
+    }
+}
